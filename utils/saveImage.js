4d85b7760904b68bbc97b4b8a1e0a277e622fb04
Tushar <tusharvaghela@gmail.com>
2020-10-21 13:11:01 +0530

    Using MD5 hash as file name

--- utils/saveImage.js
+++ utils/saveImage.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const md5 = require('md5');
 const text2png = require('text2png');
 
 const saveImage = (text) => {
   console.log(__dirname, text);
-  const fileName = __dirname.replace('utils', 'public/images/') + text + '.png';
+  const fileName = __dirname.replace('utils', 'public/images/') +
+      md5(text) + '.png';
   fs.writeFileSync(fileName, text2png(text,
       {
         color: 'teal',
